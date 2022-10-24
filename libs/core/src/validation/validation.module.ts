f3bbb9f9a03a68febb5e5d24de1ae5d125d46dd9
Nikos Oikonomou <nikoikonomou92@gmail.com>
2022-10-24 17:18:16 +0300

    core: validation: moved logic into factories

--- libs/core/src/validation/validation.module.ts
+++ libs/core/src/validation/validation.module.ts
@@ -1,23 +1,7 @@
-import { HttpStatus, Module, ValidationPipe } from '@nestjs/common';
-import { APP_PIPE } from '@nestjs/core';
-import { errorsToHttp } from './converters/errors.converter';
+import { Module } from '@nestjs/common';
+import { validationFactory } from '@app/core/validation/factories/validation.factory';
 
 @Module({
-  providers: [
-    {
-      provide: APP_PIPE,
-      useFactory: () =>
-        new ValidationPipe({
-          enableDebugMessages: process.env.NODE_ENV !== 'production',
-          whitelist: true,
-          transform: true,
-          transformOptions: {
-            enableImplicitConversion: true,
-          },
-          errorHttpStatusCode: HttpStatus.BAD_REQUEST,
-          exceptionFactory: errorsToHttp,
-        }),
-    },
-  ],
+  providers: [validationFactory],
 })
 export class ValidationModule {}
