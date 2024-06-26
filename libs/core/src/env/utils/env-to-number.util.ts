4eb797b5232ae48a83a863e0370db8670d805429
nikos <nikoikonomou92@gmail.com>
2024-06-26 17:12:30 +0300

    core: better env-to-number

--- libs/core/src/env/utils/env-to-number.util.ts
+++ libs/core/src/env/utils/env-to-number.util.ts
@@ -1,7 +1,7 @@
-export const envToNumber = (
+export const envToNumber = <T = undefined>(
   field: string,
-  defaultValue?: number,
-): number | undefined => {
+  defaultValue: T,
+): number | T => {
   const value = process.env[field] || '';
   const valueInt = Number.parseInt(value, 10);
   return Number.isFinite(valueInt) ? valueInt : defaultValue;
